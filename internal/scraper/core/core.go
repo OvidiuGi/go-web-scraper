6dc1b2f856ed8fda78adb954d683c9e461eae75b
OvidiuGi <gireada_ovidiu@yahoo.com>
2025-07-08 01:24:05 +0300

    Added allowed domains

--- internal/scraper/core/core.go
+++ internal/scraper/core/core.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"log"
+	"net/url"
 
 	"github.com/OvidiuGi/go-web-scraper/internal/scraper/parser"
 	"github.com/OvidiuGi/go-web-scraper/internal/shared/model"
@@ -13,6 +14,17 @@ func ScrapeFromSource(setting model.ScraperSettings) []model.Data {
 
 	c := colly.NewCollector()
 	childCollector := c.Clone()
+
+	domain, err := url.Parse(setting.Source)
+
+	if err != nil {
+		log.Println("Invalid source URL:", err)
+		return data
+	}
+
+	c.AllowedDomains = []string{
+		domain.Hostname(),
+	}
 
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong:", err)
